84cf4a4a0f880f90d678c09fe4dffd697f34e95c
Ismayel Hossen <ismayelhossen123@gmail.com>
2022-10-18 14:49:46 +0600

    Some functionality change

--- BBA Controller/Search.js
+++ BBA Controller/Search.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const Search_Route = express.Router();
-const mysql = require("mysql");
 const path = require("path");
 const multer = require("multer");
 const DBQuery = require("../Database/Query_Builder");
@@ -8,7 +7,7 @@ const DBQuery = require("../Database/Query_Builder");
 Search_Route.get("/search/:search", async function (req, res) {
   const s = req.params;
   console.log(s);
-  const query = `SELECT*FROM documents where lower(name) like '%${s.search}%' or document_id like '%${s.search}%' or lower(datentime) like '%${s.search}%' `;
+  const query = `SELECT*FROM documents where lower(name) like '%${s.search}%' OR meeting_id like '%${s.search}%' OR lower(datentime) like '%${s.search}%' OR lower(emp_id) like '%${s.search}%' `;
   const result = await DBQuery(query);
 
   res.status(200).json({
@@ -37,7 +36,7 @@ Search_Route.get("/all_documents_searchh/:search", async function (req, res) {
   const s = req.params;
   console.log("all");
   console.log(s);
-  const query = `SELECT fileupload.*,documents.name FROM fileupload inner join documents on fileupload.documents_id=documents.id where lower(fileupload.filename) like '%${s.search}%' OR lower(fileupload.datentime) like '%${s.search}%'  OR lower(documents.name) like '%${s.search}%'  `;
+  const query = `SELECT fileupload.*,documents.name FROM fileupload inner join documents on fileupload.documents_id=documents.id where lower(fileupload.filename) like '%${s.search}%' OR lower(fileupload.datentime)  like '%${s.search}%'  OR lower(documents.name)  like '%${s.search}%'  `;
   const result = await DBQuery(query);
 
   res.status(200).json({
