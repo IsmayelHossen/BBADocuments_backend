3e8d86c0d2cd5b20c4e5219d4ce12fa7748195ee
Ismayel Hossen <ismayelhossen123@gmail.com>
2022-10-19 11:03:27 +0600

    In view controller some query changed

--- BBA Controller/View.js
+++ BBA Controller/View.js
@@ -8,7 +8,7 @@ const DBQuery = require("../Database/Query_Builder");
 //get method
 View_Route.get("/getdata", async function (req, res) {
   const query =
-    "SELECT documents.*,view_employees.* from documents join view_employees on documents.emp_id=view_employees.emp_id order  by id";
+    "SELECT documents.*,view_employees.* from documents join view_employees on documents.emp_id=view_employees.emp_id order  by documents.id";
   const result = await DBQuery(query);
 
   res.status(200).json({
@@ -31,7 +31,7 @@ View_Route.get("/filedata/:id", async function (req, res) {
 View_Route.get("/docslist", async function (req, res) {
   const s = req.params.id;
 
-  const query = `SELECT documents.*,fileupload.*  FROM fileupload inner join documents on documents.id=fileupload.documents_id `;
+  const query = `SELECT documents.*,fileupload.*  FROM fileupload inner join documents on documents.id=fileupload.documents_id order by fileupload.id asc `;
 
   const result = await DBQuery(query);
 
